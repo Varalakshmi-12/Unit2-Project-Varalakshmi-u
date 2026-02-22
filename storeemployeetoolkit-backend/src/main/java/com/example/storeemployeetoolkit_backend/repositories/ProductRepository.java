060d4cb6f86f1de2b3f644c2461ed6b7893d5978
Varalakshmi-12 <pandu.chetan27@gmail.com>
2026-02-22 16:28:19 -0600

    ProducRepository code is added

--- storeemployeetoolkit-backend/src/main/java/com/example/storeemployeetoolkit_backend/repositories/ProductRepository.java
+++ storeemployeetoolkit-backend/src/main/java/com/example/storeemployeetoolkit_backend/repositories/ProductRepository.java
@@ -1,4 +1,7 @@
 package com.example.storeemployeetoolkit_backend.repositories;
 
-public interface ProductRepository {
+import com.example.storeemployeetoolkit_backend.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Long> {
 }
