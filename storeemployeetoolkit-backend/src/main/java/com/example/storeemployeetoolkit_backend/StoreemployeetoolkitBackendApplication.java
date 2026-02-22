a17643b93e49d7e494b851aa929bb28c185c1da3
Varalakshmi-12 <pandu.chetan27@gmail.com>
2026-02-21 21:56:11 -0600

    * first commit from IDE ,commiting pom.xml

---
+++ storeemployeetoolkit-backend/src/main/java/com/example/storeemployeetoolkit_backend/StoreemployeetoolkitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.storeemployeetoolkit_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreemployeetoolkitBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreemployeetoolkitBackendApplication.class, args);
+	}
+
+}
---
+++ storeemployeetoolkit-backend/src/test/java/com/example/storeemployeetoolkit_backend/StoreemployeetoolkitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.storeemployeetoolkit_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreemployeetoolkitBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
