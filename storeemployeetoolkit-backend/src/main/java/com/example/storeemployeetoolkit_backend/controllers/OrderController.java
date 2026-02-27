d2566640dc9da72f33c8e3fcd992597b2c444ae5
Varalakshmi-12 <pandu.chetan27@gmail.com>
2026-02-27 16:46:03 -0600

    localhost port link is added

--- storeemployeetoolkit-backend/src/main/java/com/example/storeemployeetoolkit_backend/controllers/OrderController.java
+++ storeemployeetoolkit-backend/src/main/java/com/example/storeemployeetoolkit_backend/controllers/OrderController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/orders")
-@CrossOrigin
+@CrossOrigin(origins="http://localhost:5173")
 public class OrderController {
 
     private final OrderService service;
